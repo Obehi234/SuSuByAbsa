4961ce6109979a3c8a2909e64214334cf4f6fba0
Obehi234 <obehi.ataga234@gmail.com>
2023-10-26 09:02:22 +0100

    udpated env file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,10 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+
 
 // Go to https://infura.io/, sign up, create a new API key
 // in its dashboard, and replace "KEY" with it
-const INFURA_API_KEY = "f5e57da62a49472099d94c1aedbdd114";
+const INFURA_API_KEY =  process.env.INFURA_API_KEY;
 
 // Replace this private key with your Sepolia account private key
 // To export your private key from Coinbase Wallet, go to
@@ -10,13 +12,13 @@ const INFURA_API_KEY = "f5e57da62a49472099d94c1aedbdd114";
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = "95d0c9d019793cd2bda39f93064aa60bbf64ca9abba40c22566effc5524b3cb6";
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.19",
   etherscan: {
     apiKey: {
-      sepolia: 'X37WCS9YJEDSMDD7UEPS1WZ9GS76M2XBQQ'
+      sepolia: 'APIKEY'
     }
   },
   networks: {
